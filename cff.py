6dea91cf27c54889f045e080e719dc533cd7a9f2
Diego999 <diegoantognini@gmail.com>
2020-07-31 09:53:23 +0200

    Improve stability

--- cff.py
+++ cff.py
@@ -2,7 +2,7 @@ import tqdm
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from datetime import datetime
+from datetime import datetime, timedelta
 from optparse import OptionParser
 import time
 from beautifultable import BeautifulTable
@@ -38,8 +38,14 @@ def is_first_class_ticket(driver, wait, button_idx):
         wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='mod_timetable_cta leistungOfferierenWrapper updatableForAbPreis']")))
     except:
         pass
-    button = driver.find_elements_by_xpath("//div[@class='mod_timetable_cta leistungOfferierenWrapper updatableForAbPreis']")[button_idx]
-    action.move_to_element_with_offset(button, 5, 5)
+    reiterate = True
+    while reiterate:
+        try:
+            button = driver.find_elements_by_xpath("//div[@class='mod_timetable_cta leistungOfferierenWrapper updatableForAbPreis']")[button_idx]
+            action.move_to_element_with_offset(button, 5, 5)
+            reiterate = False
+        except:
+            time.sleep(0.1)
     action.click()
     action.perform()
     try:
@@ -122,18 +128,20 @@ def find_offer(FROM, TO, DATE, TIME, NB_PREV_AFTER):
         tmp = str_time.split()
         h, m = tmp[0], tmp[2]
         return int(h)*60 + int(m)
-
+    print('Initializing the driver')
     driver = start_driver(WINDOW_SIZE)
     wait = WebDriverWait(driver, 20)
 
     driver.get("https://www.sbb.ch/en/buying/pages/fahrplan/fahrplan.xhtml")
 
+    print('Entering information')
     find_fill('shopForm_von_valueComp', FROM, driver)
     find_fill('shopForm_nach_valueComp', TO, driver)
     find_fill('shopForm_datepicker_valueComp', datetime.strftime(DATE, '%a, %d.%m.%Y'), driver)
     find_fill('shopForm_timepicker_valueComp', TIME, driver)
     driver.find_element_by_xpath("//button[@class='text__primarybutton button verbindungSuchen']").click()
 
+    print('Gathering routes')
     extend("//span[@id='verbindungsUebersicht_fruehereVerbindungenSuchen']", NB_PREV_AFTER, driver, wait)
     extend("//span[@id='verbindungsUebersicht_spaetereVerbindungenSuchen']", NB_PREV_AFTER, driver, wait)
     time.sleep(1)
@@ -165,7 +173,7 @@ def find_offer(FROM, TO, DATE, TIME, NB_PREV_AFTER):
             continue
 
         offer_dict = extract_info(text, idx, is_supersaver, driver, wait)
-        if compute_duration_in_minute(offer_dict['dur']) <= options.max_duration:
+        if compute_duration_in_minute(offer_dict['dur']) <= args.max_duration:
             offers.append(offer_dict)
 
         if len(all_dates) > 1: # Need to fix dates
@@ -209,26 +217,29 @@ def look_up_offers(FROM, TO, DATE, TIME, options):
 
 if __name__ == '__main__':
     parser = OptionParser()
+    today = datetime.now()
+    two_weeks = today + timedelta(days=14)
     parser.add_option('--nb_prev_after', type=int, default=2)
     parser.add_option('--from_station', type=str, default='Zürich HB')
     parser.add_option('--to_station', type=str, default='Neuchâtel')
-    parser.add_option('--day', type=int, default=datetime.now().day)
-    parser.add_option('--month', type=int, default=datetime.now().month)
-    parser.add_option('--year', type=int, default=datetime.now().year)
-    parser.add_option('--time', type=str, default=datetime.now().time().strftime('%H:%M'))
+    parser.add_option('--day', type=int, default=two_weeks.day)
+    parser.add_option('--month', type=int, default=two_weeks.month)
+    parser.add_option('--year', type=int, default=two_weeks.year)
+    parser.add_option('--time', type=str, default=two_weeks.time().strftime('%H:%M'))
     parser.add_option('--topk', type=int, default=15)
     parser.add_option('--max_duration', type=int, default=180)
     parser.add_option('--reversed', action="store_true", default=False)
-    (options, args) = parser.parse_args()
-    DATE = datetime(day=options.day, month=options.month, year=options.year)
+    (args, _) = parser.parse_args()
+    DATE = datetime(day=args.day, month=args.month, year=args.year)
     WINDOW_SIZE = "1920,1080"
 
-    from_station, to_station = options.from_station, options.to_station
-    if options.reversed:
+    print('Computing prices for: {} -> {} around {}/{}/{} at {} with a max duration of {} minutes. The operation will take approximately two minutes.'.format(args.from_station, args.to_station, args.day, args.month, args.year, args.time, args.max_duration))
+    from_station, to_station = args.from_station, args.to_station
+    if args.reversed:
         from_station, to_station = to_station, from_station
 
     start_time = time.time()
-    output, table = look_up_offers(from_station, to_station, DATE, options.time, options)
+    output, table = look_up_offers(from_station, to_station, DATE, args.time, args)
     print(output)
     print(table)
     stop_time = time.time()
